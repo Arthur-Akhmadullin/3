3cf20d858d94ca5e1af8737e8eed066c9368feda
Arthur Akhmadullin <akhmadullinartur@yandex.ru>
2018-12-08 22:38:35 +0500

    delete comments

--- 3.2.7.1.py
+++ 3.2.7.1.py
@@ -8,30 +8,3 @@ for files in range(1,11):
             f.write(str(random.randint(1,10)) + '\n')
         f.write(str(random.randint(1, 10)))
     f.close()
-
-
-
-'''
-file = open('file.txt', 'rt')
-s = file.readline()
-while s != '':
-    print(s.rstrip())
-    s = file.readline()
-file.close()
-
-file = open('file.txt', 'rt')
-for s in file:
-    print(s.rstrip())
-file.close()
-
-file = open('file.txt', 'at')
-file.write('\nзатем влюбилась\n')
-file.write('в дядю васю')
-file.close()
-
-file = open('file.txt', 'rt')
-for s in file:
-    print(s.rstrip())
-file.close()
-'''
-
